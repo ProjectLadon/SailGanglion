92964369c588a2a8f9a17e7ad46effe5fb792da9
Pierce Nichols <pierce@logos-electro.com>
2017-09-27 22:34:45 -0700

    Fixed excessively short delay on receive, added LED feedback and reconnection to wifi

--- Arduino/LadonSail/LadonSail.ino
+++ Arduino/LadonSail/LadonSail.ino
@@ -1,3 +1,4 @@
+
 //#define DEBUG_MODE  (1)
 
 #include <Servo.h>
@@ -33,7 +34,7 @@ char password[] = "divedivedive";
 
 // Create an instance of the server
 WiFiServer server(LISTEN_PORT);
-WiFiUDP Udp;
+//WiFiUDP Udp;
 
 // Declare functions to be exposed to the API
 int servoControl(String command);
@@ -55,6 +56,14 @@ void setup(void)
   // Start Serial
   Serial.begin(115200);
 
+  // start servo
+  sail.attach(SAIL_PIN);
+  sail.write(90);
+
+  // turn on LED output
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
   // Function to be exposed
   rest.function("sail",servoControl);
 
@@ -69,10 +78,12 @@ void setup(void)
     rest.set_id("1");
     rest.set_name("foresail");
     WiFi.config(foresailIP);
+    if (Serial) Serial.println("Foresail");
   } else {
     rest.set_id("2");
     rest.set_name("mizzen");
     WiFi.config(mizzenIP);
+    if (Serial) Serial.println("Mizzen");
   }
 
   // Connect to WiFi
@@ -81,58 +92,79 @@ void setup(void)
     if (Serial) Serial.println(ssid);
     status = WiFi.begin(ssid, password);
 
-    // Wait 10 seconds for connection:
-    delay(10000);
+    // Wait 1 seconds for connection:
+    delay(1000);
   }
   if (Serial) Serial.println("WiFi connected");
+  digitalWrite(LED_BUILTIN, HIGH);
 
   // Start the server
   server.begin();
   if (Serial) Serial.println("Server started");
-  Udp.begin(UDP_PORT);
+//  Udp.begin(UDP_PORT);
 
   // Print the IP address
   IPAddress ip = WiFi.localIP();
   if (Serial) Serial.print("IP Address: ");
   if (Serial) Serial.println(ip);
 
-  sail.attach(SAIL_PIN);
-  sail.write(90);
 }
 
 void loop() {
 
+  // check for connection, reconnect if missing
+  if (WiFi.status() == WL_CONNECTED) {
+    //digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+    if (Serial) {
+      Serial.println("Lost WiFi connection");
+      Serial.print("Wifi status is: ");
+      Serial.println(WiFi.status());
+    }
+    WiFi.begin(ssid, password);
+    delay(100);
+    return;
+  }
+
   // Handle REST calls
   WiFiClient client = server.available();
   if (!client) {
+    digitalWrite(LED_BUILTIN, LOW);
     return;
   }
   int cnt = 0;
-  while(!client.available() && (cnt < 100)){
+  while(!client.available() && (cnt < 50)) {
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(1);
     cnt++;
+  }
+  if (client.available()) {
+    if (Serial) {Serial.print("Received "); Serial.print(client.available()); Serial.println(" bytes");}
+  } else {
+    if (Serial) Serial.println("Connection established no bytes received");
   }
   rest.handle(client);
   client.stop();
 
   // Handle UDP calls
-  if (Udp.parsePacket()) {
-    IPAddress remote(Udp.remoteIP());
-    if (remote != controllerIP) {
-      if (Serial) Serial.println("UDP packet from invalid IP");
-    } else {
-      String command;
-      while (Udp.available()) {
-        command += Udp.read();
-      }
-      int cmd = command.toInt();
-      if (Serial) {
-        Serial.print("Executing UDP command: ");
-        Serial.println(cmd);
-      }
-      sailWrite(cmd);
-    }
-  }
+//  if (Udp.parsePacket()) {
+//    IPAddress remote(Udp.remoteIP());
+//    if (remote != controllerIP) {
+//      if (Serial) Serial.println("UDP packet from invalid IP");
+//    } else {
+//      String command;
+//      while (Udp.available()) {
+//        command += Udp.read();
+//      }
+//      int cmd = command.toInt();
+//      if (Serial) {
+//        Serial.print("Executing UDP command: ");
+//        Serial.println(cmd);
+//      }
+//      sailWrite(cmd);
+//    }
+//  }
   
 }
 
