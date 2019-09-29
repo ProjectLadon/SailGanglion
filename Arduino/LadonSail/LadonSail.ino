18b13f365e4f0a427c3d26a572f4a98151d81633
Pierce Nichols <pierce@logos-electro.com>
2019-09-28 18:19:46 -0700

    Up to date with latest hardware changes.

--- Arduino/LadonSail/LadonSail.ino
+++ Arduino/LadonSail/LadonSail.ino
@@ -1,5 +1,8 @@
 
-//#define DEBUG_MODE  (1)
+#define DEBUG_MODE  (1)
+
+// Get the very base of the stack...
+char* stack_start;
 
 #define ESP8266
 
@@ -7,7 +10,8 @@
 #include <SparkFunMPU9250-DMP.h>
 #include <ESP8266WiFi.h>
 #include <ros.h>
-#include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
+//#include <GDBStub.h>
 
 /* 
 * Sail mode is selected by tieing pin to gnd. Pins are pulled up internally.
@@ -20,11 +24,14 @@
 #define SAIL_SERVO_PIN  (2)
 #define COMPASS_MAG_ADDRESS (0x1e)
 #define COMPASS_ACC_ADDRESS (0x68)
-//#define AOA_PIN   (A0)
-//#define AOA_PWR   (A1)
-//#define AOA_GND   (A2)
-//#define IMU_PWR   (10)
-//#define IMU_GND   (9)
+
+// Create ROS messages and pointers for subscribers and publishers
+ros::NodeHandle*    nh = new ros::NodeHandle();
+std_msgs::Int32*  hdg_msg = new std_msgs::Int32();
+std_msgs::Int32*  tail_msg = new std_msgs::Int32();
+ros::Publisher*     hdg_publisher;
+ros::Publisher*     tail_publisher;
+ros::Subscriber<std_msgs::Int32>* cmd_subscriber;
 
 // IP Addresses
 IPAddress foresailIP(192,168,8,90);
@@ -36,68 +43,66 @@ const uint16_t ros_port = 11411;
 
 // IMU object & addresses
 #define AHRS true         // Set to false for basic data read
-MPU9250_DMP imu;
+MPU9250_DMP* imu = new MPU9250_DMP();
 bool imuGood = false;
-
-// Status
-//int status = WL_IDLE_STATUS;
-
-// Create ROS messages and pointers for subscribers and publishers
-ros::NodeHandle     nh;
-std_msgs::Float32   cmd_msg;
-std_msgs::Float32   hdg_msg;
-std_msgs::Float32   tail_msg;
-ros::Publisher*     hdg_publisher;
-ros::Publisher*     tail_publisher;
-ros::Subscriber<std_msgs::Float32>* cmd_subscriber;
 
 // WiFi parameters
 char ssid[] = "BeagleBone-DEB1";
 char password[] = "BeagleBone";
 
 // Declare functions to be exposed to the API
-void tail_callback(const std_msgs::Float32& cmd);
+void tail_callback(const std_msgs::Int32& cmd);
 
 // Declare sail write function
 bool sailWrite (int cmd);
 
 // Create Servo instance
 Servo sail;
-#define SERVO_OFFSET  (90)
+int32_t servoOffset = 90;
+#define FORESAIL_OFFSET  (90)
+#define MIZZEN_OFFSET  (90)
+#define SINGLE_OFFSET  (90)
 #define SERVO_RANGE   (45)
 
 void setup(void) {
+  // Get the start of the stack
+  char stack;
+  stack_start = &stack;
+  
   // Start Serial
   Serial.begin(115200);
+  // gdbstub_init();
 
   // start servo
   sail.attach(SAIL_SERVO_PIN);
-  sail.write(SERVO_OFFSET);
+  sail.write(servoOffset);
 
   // turn on LED output
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 
-  if (Serial) Serial.println("I live!");
+  if (DEBUG_MODE) Serial.println(F("I live!"));
     
-  if (imu.begin() != INV_SUCCESS) {
+  if (imu->begin() != INV_SUCCESS) {
     while (1)
     {
-      if (Serial) Serial.println("Unable to communicate with MPU-9250");
-      if (Serial) Serial.println("Check connections, and try again.");
-      if (Serial) Serial.println();
+      if (DEBUG_MODE) Serial.println(F("Unable to communicate with MPU-9250"));
+      if (DEBUG_MODE) Serial.println(F("Check connections, and try again."));
+      if (DEBUG_MODE) Serial.println();
       delay(5000);
     }
-  } else if (Serial) Serial.println("Connected to MPU-9250");
-  imu.dmpBegin(DMP_FEATURE_6X_LP_QUAT | // Enable 6-axis quat
+  } else if (DEBUG_MODE) Serial.println("Connected to MPU-9250");
+  if ((imu->dmpBegin(DMP_FEATURE_6X_LP_QUAT | // Enable 6-axis quat
                DMP_FEATURE_GYRO_CAL, // Use gyro calibration
-               10);
+               10) != INV_SUCCESS) && DEBUG_MODE) {
+                Serial.println("DMP start failed!");
+               }
 
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
-  // WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA);
   
   // Check if we are foresail or mizzen
   // Create ROS publishers and subscribers
@@ -107,42 +112,33 @@ void setup(void) {
   // Foresail only mode
   if (digitalRead(FORESAIL_SELECT_PIN) == LOW && digitalRead(MIZZEN_SELECT_PIN) == HIGH) {
     WiFi.config(foresailIP, ros_server, netmask);
-    static ros::Subscriber<std_msgs::Float32> tailsub("/foresail/cmd", tail_callback);
-    static ros::Publisher hdgpub("/foresail/heading", &hdg_msg);
-    static ros::Publisher tailpub("/foresail/tail", &tail_msg);
-    cmd_subscriber = &tailsub;
-    hdg_publisher = &hdgpub;
-    tail_publisher = &tailpub;
-    if (Serial) Serial.println("Foresail program pins detected");
+    cmd_subscriber = new ros::Subscriber<std_msgs::Int32>("/foresail/cmd", tail_callback);
+    hdg_publisher = new ros::Publisher("/foresail/heading", hdg_msg);
+    tail_publisher = new ros::Publisher("/foresail/tail", tail_msg);
+    if (DEBUG_MODE) Serial.println(F("Foresail program pins detected"));
+    servoOffset = FORESAIL_OFFSET;
 
   // Mizzen only mode
   } else if (digitalRead(FORESAIL_SELECT_PIN) == HIGH && digitalRead(MIZZEN_SELECT_PIN) == LOW) {
     WiFi.config(mizzenIP, ros_server, netmask);
-    static ros::Subscriber<std_msgs::Float32> tailsub("/mizzen/cmd", tail_callback);
-    static ros::Publisher hdgpub("/mizzen/heading", &hdg_msg);
-    static ros::Publisher tailpub("/mizzen/tail", &tail_msg);
-    cmd_subscriber = &tailsub;
-    hdg_publisher = &hdgpub;
-    tail_publisher = &tailpub;
-    if (Serial) Serial.println("Mizzen program pins detected");
+    cmd_subscriber = new ros::Subscriber<std_msgs::Int32>("/mizzen/cmd", tail_callback);
+    hdg_publisher = new ros::Publisher("/mizzen/heading", hdg_msg);
+    tail_publisher = new ros::Publisher("/mizzen/tail", tail_msg);
+    if (DEBUG_MODE) Serial.println(F("Mizzen program pins detected"));
+    servoOffset = MIZZEN_OFFSET;
 
   // Single-sail mode
   } else if (digitalRead(FORESAIL_SELECT_PIN) == HIGH && digitalRead(MIZZEN_SELECT_PIN) == HIGH) {
     WiFi.config(mainIP, ros_server, netmask);
-    static ros::Subscriber<std_msgs::Float32> tailsub("/sail/cmd", tail_callback);
-    static ros::Publisher hdgpub("/sail/heading", &hdg_msg);
-    static ros::Publisher tailpub("/sail/tail", &tail_msg);
-    cmd_subscriber = &tailsub;
-    hdg_publisher = &hdgpub;
-    tail_publisher = &tailpub;
-    if (Serial) Serial.println("Single-sail program pins detected");
+    cmd_subscriber = new ros::Subscriber<std_msgs::Int32>("/sail/cmd", tail_callback);
+    hdg_publisher = new ros::Publisher("/sail/heading", hdg_msg);
+    tail_publisher = new ros::Publisher("/sail/tail", tail_msg);
+    if (DEBUG_MODE) Serial.println(F("Single-sail program pins detected"));
+    servoOffset = SINGLE_OFFSET;
 
   // Illegal mode, both selection pins pulled low
   } else {
-    if (Serial) Serial.println("Illegal mode selection");
-
-    // TODO: Define failsafe behavior for illegal mode selection
-    // Loop here forever
+    if (DEBUG_MODE) Serial.println(F("Illegal mode selection"));
     for (;;) {} 
   }
 
@@ -150,87 +146,90 @@ void setup(void) {
   uint32_t wifi_conn_cnt = 0;
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    if (Serial) Serial.print("Attempting to connect to SSID: ");
-    if (Serial) Serial.print(ssid);
-    if (Serial) Serial.print(" Attempt #");
-    if (Serial) Serial.println(wifi_conn_cnt);
+    if (DEBUG_MODE) Serial.print(F("Attempting to connect to SSID: "));
+    if (DEBUG_MODE) Serial.print(ssid);
+    if (DEBUG_MODE) Serial.print(F(" Attempt #"));
+    if (DEBUG_MODE) Serial.println(wifi_conn_cnt);
     wifi_conn_cnt++;
 
     // Wait 1 seconds for connection:
     delay(1000);
   }
-  if (Serial) Serial.println("WiFi connected");
+  if (DEBUG_MODE) Serial.println(F("WiFi connected"));
   digitalWrite(LED_BUILTIN, HIGH);
 
   // Print the IP address
   IPAddress ip = WiFi.localIP();
-  if (Serial) Serial.print("IP Address: ");
-  if (Serial) Serial.println(ip);
+  if (DEBUG_MODE) Serial.print(F("IP Address: "));
+  if (DEBUG_MODE) Serial.println(ip);
 
   // Start ROS
-  nh.getHardware()->setConnection(ros_server, ros_port);
-  nh.initNode();
-  nh.advertise(*hdg_publisher);
-  nh.advertise(*tail_publisher);
-  nh.subscribe(*cmd_subscriber);
+  nh->getHardware()->setConnection(ros_server, ros_port);
+  nh->initNode();
+  nh->advertise(*hdg_publisher);
+  nh->advertise(*tail_publisher);
+  nh->subscribe(*cmd_subscriber);
 
 }
 
 void loop() {
 
   // trigger IMU processing 
-  if ( imu.fifoAvailable() ) {
+  if ( imu->fifoAvailable() ) {
     // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
-    if ( imu.dmpUpdateFifo() == INV_SUCCESS) {
+    if (DEBUG_MODE) Serial.println(F("Compass data available..."));
+    if ( imu->dmpUpdateFifo() == INV_SUCCESS) {
       // computeEulerAngles can be used -- after updating the
       // quaternion values -- to estimate roll, pitch, and yaw
-      //if (Serial) Serial.println("Getting compass data...");
-      imu.computeEulerAngles();
-      hdg_msg.data = 360 - imu.yaw;
+      if (DEBUG_MODE) Serial.println(F("Getting compass data..."));
+      imu->computeEulerAngles();
+      hdg_msg->data = 360 - imu->yaw;
+    } else if (DEBUG_MODE) {
+      Serial.print("Compass data fetch failed."); 
     }
   }
 
   // check for connection, reconnect if missing
   if (WiFi.status() == WL_CONNECTED) {
-    //digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
   } else {
     digitalWrite(LED_BUILTIN, LOW);
-    if (Serial) Serial.println("Lost WiFi connection");
-    if (Serial) Serial.print("Wifi status is: ");
-    if (Serial) Serial.println(WiFi.status());
+    if (DEBUG_MODE) Serial.println(F("Lost WiFi connection"));
+    if (DEBUG_MODE) Serial.print(F("Wifi status is: "));
+    if (DEBUG_MODE) Serial.println(WiFi.status());
     WiFi.begin(ssid, password);
     delay(100);
     return;
   }
 
   // ROS publication
-  hdg_publisher->publish(&hdg_msg);
-  tail_publisher->publish(&tail_msg);
-
+  hdg_publisher->publish(hdg_msg);
+  tail_publisher->publish(tail_msg);
+  
   // ROS Spin
-  nh.spinOnce();
+  nh->spinOnce();
   delay(50);
 
   return;
 }
 
 // Custom function accessible by the API
-void tail_callback(const std_msgs::Float32& cmd) {
+void tail_callback(const std_msgs::Int32& cmd) {
 
   // Get state from command
-  if (Serial) Serial.print("Incoming command: ");
-  if (Serial) Serial.println(cmd.data);
-  int cmd_int = (int)(cmd.data + SERVO_OFFSET);
+  if (DEBUG_MODE) Serial.print("Incoming command: ");
+  if (DEBUG_MODE) Serial.println(cmd.data);
+  int cmd_int = (int)(cmd.data + servoOffset);
   if (sailWrite(cmd_int)) {
-    tail_msg.data = cmd_int;
-  } else tail_msg.data = 0;
+   tail_msg->data = cmd_int - servoOffset;
+  } else tail_msg->data = 0;
 }
 
 // Sail writing command
 bool sailWrite(int cmd) {
-  if (cmd > (SERVO_OFFSET + SERVO_RANGE)) cmd = (SERVO_OFFSET + SERVO_RANGE);
-  if (cmd < (SERVO_OFFSET - SERVO_RANGE)) cmd = (SERVO_OFFSET - SERVO_RANGE);
-  if (Serial) Serial.print("Executing command "); if (Serial) Serial.println(cmd);
+  if (cmd > (servoOffset + SERVO_RANGE)) cmd = (servoOffset + SERVO_RANGE);
+  if (cmd < (servoOffset - SERVO_RANGE)) cmd = (servoOffset - SERVO_RANGE);
+  if (DEBUG_MODE) Serial.print("Executing command "); if (DEBUG_MODE) Serial.println(cmd);
   sail.write(cmd);
   return true;
 }
